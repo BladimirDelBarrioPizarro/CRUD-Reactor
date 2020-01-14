d772781042af7df4180595b67834f06767b0abee
DEL BARRIO PIZARRO Bladimir <b.delbarriopizarro@soprasteria.com>
2020-01-14 14:36:19 +0100

    feat:added GET /products

--- src/main/java/com/routerfunction/flux/controller/ProductController.java
+++ src/main/java/com/routerfunction/flux/controller/ProductController.java
@@ -1,12 +1,32 @@
 package com.routerfunction.flux.controller;
 
 import com.routerfunction.flux.model.Product;
+import static org.springframework.hateoas.server.reactive.WebFluxLinkBuilder.*;
+
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+
+@RestController
+@RequestMapping("/api/v1")
 public interface ProductController {
-    Flux<Product> findAll();
-    Mono<Product> findById(String id);
-    Mono<Product> save(Product product);
-    Mono<Void> delete(Product product);
+
+    @GetMapping(path="/products")
+    ResponseEntity<Flux<Product>> findAll();
+
+   /* @GetMapping(path="/products/{id}")
+    ResponseEntity<EntityModel<Mono<Product>>> findById(@PathVariable("id")String id);
+
+    @PostMapping(path = "/products")
+    ResponseEntity<EntityModel<Links>> save(@RequestBody Product product);
+
+    @PutMapping(path = "/products")
+    ResponseEntity<EntityModel<Mono<Product>>> update(@RequestBody Product product);
+
+    @DeleteMapping(path = "/products/{id}")
+    ResponseEntity<EntityModel<Links>> delete(@PathVariable("id") String id);*/
 }
--- src/main/java/com/routerfunction/flux/controller/impl/ProductControllerImpl.java
+++ src/main/java/com/routerfunction/flux/controller/impl/ProductControllerImpl.java
@@ -3,9 +3,13 @@ package com.routerfunction.flux.controller.impl;
 import com.routerfunction.flux.controller.ProductController;
 import com.routerfunction.flux.model.Product;
 import com.routerfunction.flux.service.ProductService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
 
+
+@Slf4j
 public class ProductControllerImpl implements ProductController {
 
     private ProductService productService;
@@ -14,23 +18,10 @@ public class ProductControllerImpl implements ProductController {
         this.productService = productService;
     }
 
-    @Override
-    public Flux<Product> findAll() {
-        return productService.findAll();
-    }
 
     @Override
-    public Mono<Product> findById(String id) {
-        return productService.findById(id);
-    }
-
-    @Override
-    public Mono<Product> save(Product product) {
-        return productService.save(product);
-    }
-
-    @Override
-    public Mono<Void> delete(Product product) {
-        return productService.delete(product);
+    public ResponseEntity<Flux<Product>> findAll() {
+        log.info(" -- GET /products");
+        return new ResponseEntity<>(productService.findAll(), HttpStatus.OK);
     }
 }
